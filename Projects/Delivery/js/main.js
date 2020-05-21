80a7259ef06c8f6874539906de77f6970f7fc545
Махмут <48020556+Mahmut09@users.noreply.github.com>
2020-05-21 18:04:14 +0600

    Сделана возможность оформления заказа
     добавлены модальные окна, исправлены модальные окно на мобилке, сделана валидация формы

--- Projects/Delivery/js/main.js
+++ Projects/Delivery/js/main.js
@@ -1,11 +1,14 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
 
 const cartButton = document.querySelector('#cart-button'),
       modal = document.querySelector('.modal'),
       close = document.querySelector('.close'),
       buttonAuth = document.querySelector('.button-auth'),
       modalAuth = document.querySelector('.modal-auth'),
+      modalOrder = document.querySelector('.modal-order'),
       closeAuth = document.querySelector('.close-auth'),
+      closeOrder = document.querySelector('.close-order'),
       logInForm = document.querySelector('#logInForm'),
       loginInput = document.querySelector('#login'),
       userName = document.querySelector('.user-name'),
@@ -16,7 +19,6 @@ const cartButton = document.querySelector('#cart-button'),
       menu = document.querySelector('.menu'),
       logo = document.querySelector('.logo'),
       cardsMenu = document.querySelector('.cards-menu'),
-      sectionHeading = document.querySelector('.section-heading'),
       restaurantTitle = document.querySelector('.restaurant-title'),
       rating = document.querySelector('.rating'),
       minPrice = document.querySelector('.price'),
@@ -25,13 +27,26 @@ const cartButton = document.querySelector('#cart-button'),
       inputLogin = document.querySelector('.input-login'),
       listCart = document.querySelector('.modal-body'),
       modalPrice = document.querySelector('.modal-pricetag'),
-      buttonClearCart = document.querySelector('.clear-cart');
+      buttonClearCart = document.querySelector('.clear-cart'),
+      order = document.querySelector('.order'),
+      emptyCart = document.querySelector('.empty-cart'),
+      buttonOrder = document.querySelector('.button-order'),
+      orderAddressStreet = document.querySelector('#order-address-street'),
+      orderAddressHome = document.querySelector('#order-address-home'),
+      orderFloor = document.querySelector('#order-floor'),
+      orderPorch = document.querySelector('#order-porch'),
+      orderFlatNumber = document.querySelector('#order-flat-number'),
+      buttonThanks = document.querySelector('.button-thanks'),
+      modalThanksClose = document.querySelector('.modal-thanks-close'),
+      closeThanks = document.querySelector('.close-thanks'),
+      modalThanks = document.querySelector('.modal-thanks');
 
 let login = localStorage.getItem('login');
 
 const cart = [];
 
-const loadCart = () => {
+
+const loadCart = () => {       // Локал Сторедж
 if (localStorage.getItem(login)) {
   JSON.parse(localStorage.getItem(login)).forEach(function(item) {
     cart.push(item);
@@ -39,7 +54,7 @@ if (localStorage.getItem(login)) {
 }
 };
 
-const cartStorage = () => {
+const cartStorage = () => {      // Локал Сторедж
   localStorage.setItem(login, JSON.stringify(cart))
 };
 
@@ -60,6 +75,31 @@ const valid = (str) => {
   return nameReg.test(str);
 }
 
+const validAdressStreet = (str) => {
+  const validOrderAdressStreet = /^[а-яА-Я][а-яА-Я\.]{3,25}$/;
+  return validOrderAdressStreet.test(str);
+}
+
+const validAdressHome = (str) => {
+  const validOrderAdressHome = /^[0-9]{1,3}$/;
+  return validOrderAdressHome.test(str);
+}
+
+const validFloor = (str) => {
+  const validOrderFloor = /^[0-9]{1,2}$/;
+  return validOrderFloor.test(str);
+}
+
+const validPorch = (str) => {
+  const validOrderPorch = /^[0-9]{1}$/;
+  return validOrderPorch.test(str);
+}
+
+const validFlat = (str) => {
+  const orderFlatNum = /^[0-9]{1,3}$/;
+  return orderFlatNum.test(str);
+}
+
 const toggleModal = () => {
   modal.classList.toggle('is-open');
 }
@@ -69,6 +109,11 @@ const toggleModalAuth = () => {
   loginInput.style.borderColor = '';
   loginInput.style.background = '';
 }
+
+const toggleModalOrder = () => {
+  modalOrder.classList.toggle('is-open');
+  toggleModal();
+};
 
 const returnMain = () => {
   containerPromo.classList.remove('hide');
@@ -93,8 +138,6 @@ const authorized = () => {
     returnMain();
   }
 
-  console.log('Авторизован');
-
   userName.textContent = login;
 
   buttonAuth.style.display = 'none';
@@ -108,8 +151,6 @@ const authorized = () => {
 }
 
 const notAuthorized = () => {
-  console.log('He aвторизован');
-
   const logIn = (event) => {
     event.preventDefault();
     loginInput.style.background = '';
@@ -138,7 +179,7 @@ const notAuthorized = () => {
 
 const checkAuth = () => login ? authorized() : notAuthorized();
 
-const createCardsRestaurant = ({ image, kitchen, name, price, products, stars, time_of_delivery: timeOfDelivery }) => {
+const createCardsRestaurant = ({ image, kitchen, name, price, products, stars, time_of_delivery: timeOfDelivery }) => {  // рендер карточек ресторана
 
   const card = `
         <a class="card card-restaurant" 
@@ -164,7 +205,7 @@ const createCardsRestaurant = ({ image, kitchen, name, price, products, stars, t
     cardsRestaurants.insertAdjacentHTML('beforeend', card);
 }
 
-const createCardGood = ({ description, image, name, price, id }) => {  
+const createCardGood = ({ description, image, name, price, id }) => {   // рендер карточек еды в ресторанах
   const card = document.createElement('div');
   card.className = 'card';
 
@@ -229,7 +270,7 @@ const addToCart = (event) => {
   const target = event.target;
 
   const buttonAddToCart = target.closest('.button-add-cart');
-  
+ 
   if (buttonAddToCart) {
     const card = target.closest('.card');
     const title = card.querySelector('.card-title-reg').textContent;
@@ -273,7 +314,7 @@ const renderCart = () => {
     return result +( parseFloat(item.cost) * item.count);  // parseFloat - берет строку и проверяет с 1 символа if (число) {запоминает}   (до пробела или другого символа)
   }, 0);
 
-  modalPrice.textContent = totalPrice + '₽';
+  modalPrice.textContent = totalPrice + ' ₽';
 }
 
 const changeCount = (event) => {
@@ -301,6 +342,7 @@ const closeModal = (event) => {
 
   if (target === modal) {
     toggleModal();
+    emptyCartClose();
   }
 };
 
@@ -312,52 +354,176 @@ const closeModalAuth = (event) => {
   }
 };
 
+const closeModalThanks = (event) => {
+  const target = event.target;
+
+  if (target === modalThanks) {
+    toggleModalThanks();
+  }
+}
+
+const closeModalOrder = (event) => {
+  const target = event.target;
+
+  if (target === modalOrder) {
+    toggleModalOrder();
+  }
+};
+
+const emptyCartClose = () => {
+  emptyCart.style.display = 'none';
+};
+
+const toggleModalThanks = () => {
+  modalThanksClose.classList.toggle('is-open');
+}
+
+const pushOrder = (event) => {         //формирует заказ
+  event.preventDefault();
+
+  const finalOrder = [];
+
+  const checkAdressStreet = validAdressStreet(orderAddressStreet.value);
+  const checkAdressHome = validAdressHome(orderAddressHome.value);
+  const checkFloor = validFloor(orderFloor.value);
+  const checkPorch = validPorch(orderPorch.value);
+  const checkFlat = validFlat(orderFlatNumber.value);
+
+
+  if (checkAdressStreet) {
+    orderAddressStreet.style.background = '';
+  }
+  if (checkFloor) {
+    orderFloor.style.background = '';
+  }
+  if (checkFlat && orderFloor.value > 0) {
+    orderFlatNumber.style.background = '';
+  }
+  if (checkPorch) {
+    orderPorch.style.background = '';
+  }
+  if (checkAdressHome) {
+    orderAddressHome.style.background = '';
+  }
+
+  if (checkAdressStreet && checkFloor && checkFlat && checkPorch && checkAdressHome) {
+
+      const totalPrice = cart.reduce(function(result, item) {         
+        return result +( parseFloat(item.cost) * item.count); 
+      }, 0);
+
+      modalPrice.textContent = totalPrice + ' ₽';
+
+      finalOrder.push('Логин: ' + login,
+                    'Заказ: '  + JSON.stringify(cart),
+                    'Сумма заказа: ' + totalPrice,
+                    'Адрес:',
+                    ' Улица: ' + orderAddressStreet.value,
+                    ' Дом: ' + orderAddressHome.value,
+                    ' Подъезд: ' + orderPorch.value,
+                    ' Этаж: ' + orderFloor.value,
+                    ' Номер квартиры: ' + orderFlatNumber.value);
+
+      for ( let key in finalOrder) {
+       let orderForPush = (finalOrder[key]);
+       console.log(orderForPush);
+      }
+      toggleModalOrder();
+      toggleModal();
+      toggleModalThanks();
+
+      orderAddressStreet.value = '';
+      orderAddressHome.value = '';
+      orderFloor.value = '';
+      orderPorch.value = '';
+      orderFlatNumber.value = '';
+
+  } else {
+    if (!checkAdressStreet) {
+      orderAddressStreet.style.background = '#ff00001f';
+    }
+    if (!checkAdressHome) {
+      orderAddressHome.style.background = '#ff00001f';
+    }
+    if (!checkFloor) {
+      orderFloor.style.background = '#ff00001f';
+    }
+    if (!checkPorch) {
+      orderPorch.style.background = '#ff00001f';
+    }
+    if (!checkFlat) {
+      orderFlatNumber.style.background = '#ff00001f';
+    }
+  }
+};
+
 function init() {
-  getData('./db/partners.json')
+  getData('./db/partners.json')       // Рендер ресторанов
     .then((data) => {
-      data.forEach(createCardsRestaurant)
+      data.forEach(createCardsRestaurant)     
   });
   
-  cartButton.addEventListener('click', toggleModal);
+  cartButton.addEventListener('click', toggleModal); // Открыть/Закрыть Корзину
 
-  cartButton.addEventListener('click', renderCart);
+  cartButton.addEventListener('click', renderCart);  // Рендер заказа в корзине
   
-  close.addEventListener('click', toggleModal);
+  close.addEventListener('click', toggleModal);    // Закрыть модальное окно
+
+  close.addEventListener('click', emptyCartClose); // Закрыть корзину если она пуста
   
-  cardsRestaurants.addEventListener('click', openGoods);
+  cardsRestaurants.addEventListener('click', openGoods); // Открывает 
 
-  cardsMenu.addEventListener('click', addToCart);
+  cardsMenu.addEventListener('click', addToCart); // Добавляет товары в корзину
 
-  listCart.addEventListener('click', changeCount);
+  listCart.addEventListener('click', changeCount); // Считает сумму в корзине
 
-  modalAuth.addEventListener('click', closeModalAuth);
+  modalAuth.addEventListener('click', closeModalAuth); // Открыть/закрыть окно авторизации
 
-  modal.addEventListener('click', closeModal);
+  modal.addEventListener('click', closeModal); // Закрывает модалку по крестику
 
-  buttonClearCart.addEventListener('click', () => {
+  closeOrder.addEventListener('click', toggleModalOrder); // Открывает/закрывает модалку оформления заказа по крестику
+
+  modalOrder.addEventListener('click', closeModalOrder); // Открывает/закрывает модалку оформления заказа 
+
+  buttonOrder.addEventListener('click', pushOrder);  // Отправляет заказ
+
+  buttonThanks.addEventListener('click', toggleModalThanks); // Открывает/закрывает окно благодарности
+
+  closeThanks.addEventListener('click', toggleModalThanks); // Открывает/закрывает окно благодарности по крестику
+
+  modalThanks.addEventListener('click', closeModalThanks); // Закрывает окно благодарности
+
+  buttonClearCart.addEventListener('click', () => { // Отчищает корзину или закрывает ее если она уже пустая
     if (cart.length === 0) {
       toggleModal();
+      emptyCartClose();
     }
     cart.length = 0;
     renderCart();
     cartStorage();
   })
+
+  order.addEventListener('click', () => { // Появление надписи если корзина пуста и пытаются формировать заказ
+    if (cart.length === 0) {
+      emptyCart.style.display = 'block';
+    } else {
+      toggleModalOrder();
+    }
+  });
   
-  logo.addEventListener('click', () => {
+  logo.addEventListener('click', () => {      // Возврат на гл страницу
       containerPromo.classList.remove('hide')
       restaurants.classList.remove('hide')
       menu.classList.add('hide')
   })
 
-  inputSearch.addEventListener('keydown', (event) => {
+  inputSearch.addEventListener('keydown', (event) => {      //Поиск
     const target = event.target;
     const value = target.value.toLowerCase();
     const goods = [];
-    
+
     if (event.keyCode === 13) {
       target.value = '';
-      
-      
 
       getData('./db/partners.json')
         .then(function(data) {                // Запрос на "сервер"
@@ -399,9 +565,8 @@ function init() {
     }
   });
 
-
 checkAuth();
-  
+
 new Swiper('.swiper-container', {
     loop: true,
     autoplay: true,
@@ -409,3 +574,4 @@ new Swiper('.swiper-container', {
 }
 
 init();
+});
